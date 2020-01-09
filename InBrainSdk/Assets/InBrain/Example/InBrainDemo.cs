1ef3f58a0006f4d9322350cd2a73f7176d7bd3e3
Ivan Tustanivskyi <itustanivsky@ninevastudios.com>
2020-01-09 10:10:07 +0200

    extract methods for calling stuff from android lib

--- InBrainSdk/Assets/InBrain/Example/InBrainDemo.cs
+++ InBrainSdk/Assets/InBrain/Example/InBrainDemo.cs
@@ -4,34 +4,21 @@ using UnityEngine;
 
 public class InBrainDemo : MonoBehaviour
 {
-	// Start is called before the first frame update
+	[SerializeField]
+	string ClientId = "9c367c28-c8a4-498d-bf22-1f3682fc73aa"; // your client id obtained by your account manager
+	
+	[SerializeField]
+	string ClientSecret = "90MB8WyMZyYykgs0TaR21SqCcCZz3YTTXio9FoN5o5NJ6+svp3Q2tO8pvM9CjbskCaLAog0msmVTcIigKPQw4A=="; // your client secret obtained by your account manager
+	
 	void Start()
 	{
-		using (var inBrain = new AndroidJavaClass("com.inbrain.sdk.InBrain"))
-		{
-			var inBrainInst = inBrain.CallStatic<AndroidJavaObject>("getInstance");
-			inBrainInst.Call("init", InBrainAndroidUtils.Activity, CLIENT_ID, CLIENT_SECRET);
-			inBrainInst.Call("addCallback", new InBrainCallbackProxy());
-		}
+		Debug.Log("Demo start!");
+		InBrain.Init(ClientId, ClientSecret);
 	}
-
-	// Update is called once per frame
-	void Update()
-	{
-	}
-
-	const string CLIENT_ID = "9c367c28-c8a4-498d-bf22-1f3682fc73aa"; // your client id obtained by your account manager
-	const string CLIENT_SECRET = "90MB8WyMZyYykgs0TaR21SqCcCZz3YTTXio9FoN5o5NJ6+svp3Q2tO8pvM9CjbskCaLAog0msmVTcIigKPQw4A=="; // your client secret obtained by your account manager
 
 	public void OnShowSurveysClicked()
 	{
-		Debug.Log("ShowSurveys clicked!");
-
-		using (var inBrain = new AndroidJavaClass("com.inbrain.sdk.InBrain"))
-		{
-			var inBrainInst = inBrain.CallStatic<AndroidJavaObject>("getInstance");
-
-			inBrainInst.Call("showSurveys", InBrainAndroidUtils.Activity);
-		}
+		Debug.Log("ShowSurveys button clicked!");
+		InBrain.ShowSurveys();
 	}
 }
--- InBrainSdk/Assets/InBrain/Scripts/API/InBrain.cs
+++ InBrainSdk/Assets/InBrain/Scripts/API/InBrain.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public static class InBrain
 {
+	public static void Init(string clientId, string clientSecret)
+	{
+		using (var inBrain = new AndroidJavaClass("com.inbrain.sdk.InBrain"))
+		{
+			var inBrainInst = inBrain.CallStatic<AndroidJavaObject>("getInstance");
+			inBrainInst.Call("init", InBrainAndroidUtils.Activity, clientId, clientSecret);
+			inBrainInst.Call("addCallback", new InBrainCallbackProxy());
+		}
+	}
 
+	public static void AddCallback()
+	{
+		using (var inBrain = new AndroidJavaClass("com.inbrain.sdk.InBrain"))
+		{
+			var inBrainInst = inBrain.CallStatic<AndroidJavaObject>("getInstance");
+			inBrainInst.Call("addCallback", new InBrainCallbackProxy());
+		}
+	}
+
+	public static void ShowSurveys()
+	{
+		using (var inBrain = new AndroidJavaClass("com.inbrain.sdk.InBrain"))
+		{
+			var inBrainInst = inBrain.CallStatic<AndroidJavaObject>("getInstance");
+
+			inBrainInst.Call("showSurveys", InBrainAndroidUtils.Activity);
+		}
+	}
 }
