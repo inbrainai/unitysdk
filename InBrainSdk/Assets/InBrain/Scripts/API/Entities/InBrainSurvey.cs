4ceb0b838dda0814b093fab662de7ee98ef914f5
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-03-05 18:18:12 +0200

    update Android native library

--- InBrainSdk/Assets/InBrain/Scripts/API/Entities/InBrainSurvey.cs
+++ InBrainSdk/Assets/InBrain/Scripts/API/Entities/InBrainSurvey.cs
@@ -10,13 +10,17 @@ namespace InBrain
 		[SerializeField] public long rank;
 		[SerializeField] public long time;
 		[SerializeField] public float value;
+		[SerializeField] public bool currencySale;
+		[SerializeField] public float multiplier;
 
-		public InBrainSurvey(string id, long rank, long time, float value)
+		public InBrainSurvey(string id, long rank, long time, float value, bool currencySale, float multiplier)
 		{
 			this.id = id;
 			this.rank = rank;
 			this.time = time;
 			this.value = value;
+			this.currencySale = currencySale;
+			this.multiplier = multiplier;
 		}
 
 		public static InBrainSurvey FromAJO(AndroidJavaObject ajo)
@@ -24,19 +28,21 @@ namespace InBrain
 			return new InBrainSurvey(ajo.Get<string>("id"),
 				ajo.Get<long>("rank"),
 				ajo.Get<long>("time"),
-				ajo.Get<float>("value"));
+				ajo.Get<float>("value"),
+				ajo.Get<bool>("currencySale"),
+				ajo.Get<float>("multiplier"));
 		}
 
 		public AndroidJavaObject ToAJO()
 		{
 			return Application.platform == RuntimePlatform.Android
-				? new AndroidJavaObject("com.inbrain.sdk.model.Survey", id, rank, time, value)
+				? new AndroidJavaObject("com.inbrain.sdk.model.Survey", id, rank, time, value, currencySale, multiplier)
 				: null;
 		}
 
 		public override string ToString()
 		{
-			return string.Format("id: {0}, rank: {1}, time: {2}, value: {3}", id, rank, time, value);
+			return string.Format("id: {0}, rank: {1}, time: {2}, value: {3}, currencySale: {4}, multiplier: {5}", id, rank, time, value, currencySale, multiplier);
 		}
 	}
 }
